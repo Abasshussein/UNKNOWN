629dd88279b43be89b0dc60da483a5c09bc1585c
Abasshussein <abasshussein03@gmail.com>
2020-02-20 22:37:18 +0300

    Modified file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function enter() {
     var input=prompt("Enter word here")
     var arr=input.slice(0,input.length-1)
-    var last=parseInt(input.chartAt(input.length-1))
+    var last=parseInt(input.charAt(input.length-1))
     if (isNaN(last)){
         alert(input+1)
     } else {
